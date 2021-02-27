e29d8788f92393c9b2296da95fa9fe9dff87adba
StevenGarP <79319259+StevenGarP@users.noreply.github.com>
2021-02-27 10:10:14 -0600

    Add files via upload

---
+++ dos ejercicios/factorial de un numero/factorial de un numero/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace factorial_de_un_numero
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            Console.WriteLine("Calculo del Factorial de un número entero positivo\n");
+            do
+            {
+                Console.Write("Por favor Digite un número entero positivo: ");
+                num = int.Parse(Console.ReadLine());
+                verificar(num);
+            } while (num < 0);
+            factorial(num);
+        }
+        static void verificar(int n)
+        {
+            if (n < 0)
+            {
+                Console.Write("\nError: No existe el factorial de un número negativo...\n");
+            }
+        }
+        static void factorial(int n)
+        {
+            int fact = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                fact *= i;
+            }
+            Console.WriteLine("\nEl Factorial de {0} es: {1}", n, fact);
+        }
+    }
+}
---
+++ dos ejercicios/fibonacci/fibonacci/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, num, i, auxiliar;
+            Console.Write("Ingrese el número de numeros de Fibonacci que desea mostrar: ");
+            num = int.Parse(Console.ReadLine());
+            a = 0;
+            b = 1;
+            for (i = 0; i < num; i++)
+            {
+                auxiliar = a;
+                a = b;
+                b = auxiliar + a;
+                Console.WriteLine(a);
+            }
+        }
+    }
+}
